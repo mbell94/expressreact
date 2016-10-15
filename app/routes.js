9c0fc33608e68a427cfa9f6434232ec8763f6323
mbell94 <bellmattheww@gmail.com>
2016-10-14 22:39:17 -0500

    Included passport/connect-flash, update project

--- app/routes.js
+++ app/routes.js
@@ -1,50 +1,31 @@
 var User = require('./model/user');
 
-module.exports = function(app, helloWorld) {
+module.exports = function(app, passport) {
     app.get('/', function(req, res) {
         console.log(req.cookies);
         console.log("=========================================================");
         console.log(req.session);
-        var drinks = [
-            { name: 'Bloody Mary', drunkness: 3 },
-            { name: 'Martini', drunkness: 5 },
-            { name: 'Scotch', drunkness: 10 }
-        ];
-        var tagline = "Any code of your own that you haven't looked at for six or more months " +
-            "might as well have been written by someone else.";
-
-        helloWorld('Javascript', function(err, data) {
-            if (err)
-                throw err;
-            else
-                console.log(data);
-        });
-
-        res.render('pages/index', {
-            /* drinks: drinks,
-            tagline: tagline */
-        });
-    })
+        res.render('pages/index');
+    });
 
     app.get('/about', function(req, res) {
         res.render('pages/about');
     });
 
-    app.get('/login/:username/:password', function(req, res) {
-        // var newUser = new User();
-        // newUser.local.username = req.params.username;
-        // newUser.local.password = req.params.password;
-        // console.log(newUser.local.username + " " + newUser.local.password);
-        // newUser.save(function(err) {
-        //     if (err)
-        //         throw err;
-        //     else
-        //         res.end('Success!');
-        // });
+    app.get('/:username/:password', function(req, res) {
+        var newUser = new User();
+        newUser.local.username = req.params.username;
+        newUser.local.password = req.params.password;
+        console.log(newUser.local.username + " " + newUser.local.password);
+        newUser.save(function(err) {
+            if (err)
+                throw err;
+            else
+                res.end('Success!');
+        });
     });
 
     app.get('/login', function(req, res) {
-
         // render the page and pass in any flash data if it exists
         res.render('pages/login.ejs', { message: req.flash('loginMessage') });
     });
@@ -57,33 +38,27 @@ module.exports = function(app, helloWorld) {
     // =====================================
     // show the signup form
     app.get('/signup', function(req, res) {
-
         // render the page and pass in any flash data if it exists
-        res.render('./signup.ejs', { message: "" /*req.flash('signupMessage')*/ });
+        res.render('./signup.ejs', { message: req.flash('signupMessage') });
     });
 
     // process the signup form
-    app.post('/signup', function(req, res) {
-        var newUser = new User();
-        newUser.local.username = req.body.email;
-        newUser.local.password = req.body.password;
-        newUser.save(function(err) {
-            if (err)
-                throw err;
-        });
-        res.redirect('pages/');
-    });
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/',
+        failureRedirect: '/signup',
+        failureFlash: true
+    }));
 
     // =====================================
     // PROFILE SECTION =====================
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('pages/profile.ejs', {
-            user: req.user // get the user out of session and pass to template
-        });
-    });
+    // app.get('/profile', isLoggedIn, function(req, res) {
+    //     res.render('pages/profile.ejs', {
+    //         user: req.user // get the user out of session and pass to template
+    //     });
+    // });
 
     // =====================================
     // LOGOUT ==============================
@@ -95,12 +70,11 @@ module.exports = function(app, helloWorld) {
 }
 
 // route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-}
+// function isLoggedIn(req, res, next) {
+//     // if user is authenticated in the session, carry on 
+//     if (req.isAuthenticated())
+//         return next();
+//     else
+//     // if they aren't redirect them to the home page
+//         res.redirect('/');
+// }
--- server.js
+++ server.js
@@ -8,53 +8,36 @@ var uuid = require('node-uuid');
 var edge = require('edge');
 var mongo = require('mongoose');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+
 var configdb = require('./config/database');
 
 mongo.connect(configdb.url);
+require('./config/passport')(passport);
 
-var helloWorld = edge.func(`
-    async (input) => { 
-        return ".NET Welcomes " + input.ToString(); 
-    }
-`);
-
-app.set('view engine', 'ejs');
+// var helloWorld = edge.func(`
+//     async (input) => { 
+//         return ".NET Welcomes " + input.ToString(); 
+//     }
+// `);
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
     secret: uuid.v1(),
     saveUninitialized: true,
     resave: true
 }));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + '/styles'));
 
-// app.get('/', function (req, res) {
-//     console.log(req.cookies);
-//     console.log("=========================================================");
-//     console.log(req.session);
-//     var drinks = [
-//         { name: 'Bloody Mary', drunkness: 3 },
-//         { name: 'Martini', drunkness: 5 },
-//         { name: 'Scotch', drunkness: 10 }
-//     ];
-//     var tagline = "Any code of your own that you haven't looked at for six or more months " +
-//                   "might as well have been written by someone else.";
-
-//     helloWorld('Javascript', function (err, data) {
-//         if(err)
-//             console.log(err);
-//         else
-//             console.log(data);
-//     });
-
-//     res.render('pages/index', {
-//         drinks: drinks,
-//         tagline: tagline
-//     });
-// });
-
-require('./app/routes.js')(app, helloWorld);
+require('./app/routes.js')(app, passport);
 
 app.listen(port);
 console.log('SERVER LISTENING ON PORT: ' + port);
